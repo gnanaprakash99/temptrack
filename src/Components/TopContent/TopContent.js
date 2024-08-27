a5e5010c20f94b7beeec6249aeb7bfca7493208a
ajay1931 <ajayprakash032@gmail.com>
2024-08-27 15:45:55 +0530

    TopContent.js

--- src/Components/TopContent/TopContent.js
+++ src/Components/TopContent/TopContent.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './TopContent.css';
 
+
 const TopContent = ({ location }) => {
   const [weather, setWeather] = useState('');
-  const apiKey = '885e5a63877a215f293e713a946e66db'; 
+  const apiKey = '885e5a63877a215f293e713a946e66db';
 
   useEffect(() => {
     const fetchWeather = async () => {
@@ -11,9 +12,9 @@ const TopContent = ({ location }) => {
         const response = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
         );
-        
+
         const data = await response.json();
-        
+
         setWeather(`${data.main.temp} °C`);
       } catch (error) {
         console.error('Error fetching weather data:', error);
@@ -28,8 +29,14 @@ const TopContent = ({ location }) => {
 
   return (
     <div className='topcontent'>
-      <h1>Today's Weather in {location}</h1>
-      <h1 className='weatherValue'>{weather}</h1>
+      <div className='topcontent_left'>
+        
+      </div>
+      <div className='topcontent_right'>
+        <h1>Today's Weather in </h1>
+        <h1>{location}</h1>
+        <h1 className='weatherValue'>{weather}</h1>
+      </div>
     </div>
   );
 }
