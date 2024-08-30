23c7c6db4eda59917742119969e6199324e4d44a
ajay1931 <ajayprakash032@gmail.com>
2024-08-30 12:10:31 +0530

    update TopGlobeRotating.js

---
+++ src/Components/TopGlobeRotating/TopGlobeRotating.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopGlobeRotating.css'; 
+
+const TopGlobeRotating = () => {
+  return (
+    <div className='topGlobeRotating'>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/7/7f/Rotating_earth_animated_transparent.gif' />
+    </div>
+  );
+}
+
+export default TopGlobeRotating;
