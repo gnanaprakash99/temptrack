6b4ecba7ea38bade815a46f5ed825c9176310e4e
ajay1931 <ajayprakash032@gmail.com>
2024-10-07 13:03:06 +0530

    Fixed lint warnings

--- src/Components/TopGlobeRotating/TopGlobeRotating.js
+++ src/Components/TopGlobeRotating/TopGlobeRotating.js
@@ -4,7 +4,7 @@ import './TopGlobeRotating.css';
 const TopGlobeRotating = () => {
   return (
     <div className='topGlobeRotating'>
-      <img src='https://upload.wikimedia.org/wikipedia/commons/7/7f/Rotating_earth_animated_transparent.gif' />
+      <img src='https://upload.wikimedia.org/wikipedia/commons/7/7f/Rotating_earth_animated_transparent.gif' alt='globe' />
     </div>
   );
 }
